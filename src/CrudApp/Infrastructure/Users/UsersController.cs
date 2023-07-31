510e3afda47618f62b482da1090b972799b23c90
Per Fuglsang Møller <pfm@commentor.dk>
2023-07-31 16:29:34 +0200

    Create UsersController.cs

---
+++ src/CrudApp/Infrastructure/Users/UsersController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CrudApp.Infrastructure.Users;
+
+public class UsersController : EntityControllerBase<User>
+{
+    [HttpGet("current")]
+    public async Task<User?> GetCurrentUser()
+    {
+        var userId = AuthenticationContext.Current?.UserId;
+        if (!userId.HasValue)
+            return default;
+
+        var user = await DbContext.GetByIdAuthorized<User>(userId.Value, asNoTracking: true);
+        return user;
+    }
+}
