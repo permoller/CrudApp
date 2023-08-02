44fe2fb36ffd10ac79f4c57a78e86e51aba805b9
Per Fuglsang Møller <pfm@commentor.dk>
2023-08-02 14:42:00 +0200

    Temporarily disable authorization code

--- src/CrudApp/Infrastructure/Authorization/AuthorizedEntitiesQuery.cs
+++ src/CrudApp/Infrastructure/Authorization/AuthorizedEntitiesQuery.cs
@@ -6,12 +6,14 @@ public static class AuthorizedEntitiesQuery
 {
     public static IQueryable<T> Authorized<T>(this CrudAppDbContext dbContext, bool includeSoftDeleted = false) where T : EntityBase
     {
-        var userId = AuthorizationContext.Current?.UserId ?? AuthenticationContext.Current?.UserId ?? throw new NotAuthenticatedException();
+        return dbContext.All<T>(includeSoftDeleted);
+        // TODO: Include this when authorization data is actually created... no no one has access to enything
+        //var userId = AuthorizationContext.Current?.UserId ?? AuthenticationContext.Current?.UserId ?? throw new NotAuthenticatedException();
 
-        var authorizedEntityIds = dbContext.All<AuthorizationGroupUserRelation>().Where(m => m.UserId == userId)
-            .SelectMany(m => m.AuthorizationGroup!.AuthorizationGroupEntityRelations!.Select(e => e.Id));
+        //var authorizedEntityIds = dbContext.All<AuthorizationGroupUserRelation>().Where(m => m.UserId == userId)
+        //    .SelectMany(m => m.AuthorizationGroup!.AuthorizationGroupEntityRelations!.Select(e => e.Id));
 
-        return dbContext.All<T>(includeSoftDeleted).Where(t => authorizedEntityIds.Contains(t.Id));
+        //return dbContext.All<T>(includeSoftDeleted).Where(t => authorizedEntityIds.Contains(t.Id));
     }
 
     public static async Task<T> GetByIdAuthorized<T>(this CrudAppDbContext dbContext, EntityId id, bool asNoTracking) where T : EntityBase
