b520fde0ec1b7b036a137bbf66ea334927394c57
Per Fuglsang Møller <pfm@commentor.dk>
2023-08-02 10:52:22 +0200

    Better support for inheritance and nullable reference types in OpenAPI documentation

--- src/CrudApp/Infrastructure/OpenApi/OpenApiServiceCollectionExtensions.cs
+++ src/CrudApp/Infrastructure/OpenApi/OpenApiServiceCollectionExtensions.cs
@@ -15,12 +15,19 @@ public static class OpenApiServiceCollectionExtensions
 
         services.AddSwaggerGen(swaggerGenOptions =>
         {
-            // We have nullable-reference-types enabled in the project.
+            // Maintain inheritance information in the OpenAPI document.
+            swaggerGenOptions.UseAllOfForInheritance();
+
+            // Support marking reference-properties (properties that contain an object) as nullable in the OpenAPI document.
+            swaggerGenOptions.UseAllOfToExtendReferenceSchemas();
+
+            // We have nullable reference types enabled in the project.
             // So properties where the type is a reference type that is not marked as nullable,
             // should also not be marked as nullable in the OpenAPI document.
             swaggerGenOptions.SupportNonNullableReferenceTypes();
-
-            // Make sure non-nullable properties are marked as required (meaning they must appear in the JSON).
+            
+            // Make sure non-nullable properties are marked as required (meaning they must appear in the JSON),
+            // to require less checks for undefined in TypeScript when coding agains generated types.
             swaggerGenOptions.SchemaFilter<RequireNonNullablePropertiesSchemaFilter>();
         });
 
