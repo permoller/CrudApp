a9d1012902bd0b07aaec9aacc4192241475d00cb
Per Fuglsang Møller <pfm@solita.dk>
2024-03-15 10:19:56 +0100

    Update HttpRequestLogging.cs

--- src/CrudApp/Infrastructure/Http/HttpRequestLogging.cs
+++ src/CrudApp/Infrastructure/Http/HttpRequestLogging.cs
@@ -5,7 +5,7 @@ public static partial class HttpRequestLogging
     [LoggerMessage(
         EventId = 1,
         Level = LogLevel.Information,
-        Message = "{Direction} HTTP request completed {Method} {Scheme}://{Host}{PathBase}{Path}{Query}{Fragment} -> {StatusCode} in {ElapsedMilliseconds}ms")]
+        Message = "{Direction} HTTP request completed {Method} {Scheme}://{Host}{PathBase}{Path}{Query}{Fragment} with status {StatusCode} in {ElapsedMilliseconds}ms")]
     public static partial void HttpRequestCompleted(
         ILogger logger,
         string direction,
