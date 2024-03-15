5318e34cfc28de882c4b3cafe10b12a40e565293
Per Fuglsang Møller <pfm@solita.dk>
2024-03-15 10:20:30 +0100

    Add "NotSet" as the default DatabaseType

--- src/CrudApp/Infrastructure/Database/DatabaseOptions.cs
+++ src/CrudApp/Infrastructure/Database/DatabaseOptions.cs
@@ -2,7 +2,7 @@
 
 namespace CrudApp.Infrastructure.Database;
 
-public enum DatabaseType { Sqlite, Postgres, MsSql, MySql }
+public enum DatabaseType { NotSet, Sqlite, Postgres, MsSql, MySql }
 
 public class DatabaseOptions
 {
