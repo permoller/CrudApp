136aa79bd6c6e768b879dd07eb1da32e1d6d7f4c
Per Fuglsang Møller <pfm@solita.dk>
2024-04-01 21:43:16 +0200

    Move Base32 functionality to UtilityCode

--- src/CrudApp/Infrastructure/Primitives/Error.cs
+++ src/CrudApp/Infrastructure/Primitives/Error.cs
@@ -12,7 +12,7 @@ public abstract partial class Error
         DataDictionary? data = default,
         Dictionary<string, string[]>? errors = default)
     {
-        Instance = GetRandomString(length: 8);
+        Instance = Base32ForHumans.GetRandomString(length: 8);
         HttpStatucCode = httpStatucCode;
         Exception = exception;
         TraceId = Activity.Current?.Id;
@@ -48,20 +48,5 @@ public abstract partial class Error
         list.Add(error);
         Errors[field] = list.ToArray();
         return this;
-    }
-
-    // Characters that are easy for one person to tell another person.
-    // Only lower case characters
-    // No numbers or characters the looks like each other (i, 1, o and 0)
-    private static readonly string _base32Alphabet = "abcdefghjklmnpqrstuvwxyz23456789";
-    private static readonly Random _random = new Random();
-
-    private static string GetRandomString(int length)
-    {
-        var resultChars = new char[length];
-        for (int i = 0; i < resultChars.Length; i++)
-            resultChars[i] = _base32Alphabet[_random.Next(_base32Alphabet.Length)];
-
-        return new(resultChars);
     }
 }
---
+++ src/CrudApp/Infrastructure/UtilityCode/Base32ForHumans.cs
@@ -0,0 +1,19 @@
+﻿namespace CrudApp.Infrastructure.UtilityCode;
+
+public static class Base32ForHumans
+{
+    // Characters that are easy for a person to read and enter or tell another person.
+    // Only lower case characters.
+    // No numbers or characters the looks like each other (l, 1, o and 0)
+    private static readonly string _base32Alphabet = "abcdefghijkmnpqrstuvwxyz23456789";
+    private static readonly Random _random = new Random();
+
+    public static string GetRandomString(int length)
+    {
+        var resultChars = new char[length];
+        for (int i = 0; i < resultChars.Length; i++)
+            resultChars[i] = _base32Alphabet[_random.Next(_base32Alphabet.Length)];
+
+        return new(resultChars);
+    }
+}
